d1bf05d510ce45c77b256a8645b2f864c2cefab9
JetQin <qinjet@gmail.com>
2016-09-09 00:26:03 +0800

    initialize tdocker project

---
+++ src/main/java/com/twigacenter/TdockerApplication.java
@@ -0,0 +1,12 @@
+package com.twigacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdockerApplication.class, args);
+	}
+}
